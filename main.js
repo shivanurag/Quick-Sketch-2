087228b7e78b3a4c8977388b4be386dd657ebd5a
shivanurag <77777785+shivanurag@users.noreply.github.com>
2023-05-10 16:13:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+quick_draw_data_set=['pen','paper','book','bottle'];
+random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+console.log(quick_draw_data_set[random_no]);
+sketch=quick_draw_data_set[random_no];
+document.getElementById("sketch_name").innerHTML=sketch;
+timer_counter=0;
+timer_check="";
+drawn_sketch="";
+answer_holder="";
+score=0;
